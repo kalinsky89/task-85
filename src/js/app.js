3b0977110ca889f6d7aa399fecf7c305b8bdbc86
Kalin Atanasov <kalin.natanasov@gmail.com>
2023-01-27 16:53:11 +0200

    task-85

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,29 @@
 import "../scss/app.scss";
-
+import 'regenerator-runtime/runtime'
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
   const ul = document.querySelector("ul");
+  
+});
+
+  
+async function pokemon(){
+  let responce = await fetch("https://pokeapi.co/api/v2/pokemon?limit=10").then((responce)=> responce.json());
+
+  // console.log(responce);
+  return responce; 
+}
+
+let pokemons = await pokemon();
+// let item = 
+// console.log(pokemons.results);
+
+pokemons.results.forEach(item => {
+  let name= item.name;
+  let pokemonItem = document.createElement("li");
+  pokemonItem.innerText= name;
+  console.log(name);
+  document.querySelector('ul').append(pokemonItem);
+  
 });
